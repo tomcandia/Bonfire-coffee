c17229e9aa0ec8df0fb2b8eb85f80945f18b4dbf
Juan David Cardona <rocandante@gmail.com>
2012-07-19 12:05:27 -0500

    traduciendo y adaptando la vista home

--- bonfire/application/views/home/index.php
+++ bonfire/application/views/home/index.php
@@ -2,22 +2,23 @@
 <div class="container">
 
 	<div class="hero-unit">
-		<h1>Welcome to Bonfire.</h1>
+		<h1>Bienvenido a <br /> Bonfire-coffee.</h1>
 
-		<p>Kickstart your CodeIgniter applications.</p>
+		<p>Ponga en marcha sus aplicaciones basadas en <br /> CodeIgniter completamente en espa&ntilde;ol latino.</p>
 	</div>
 
-<p>If you're new to Bonfire, but familiar with <a href="http://www.codeigniter.com" target="_blank">CodeIgniter</a>, then you should be up an running within the system in no time.</p>
+<p>Si eres nuevo en Bonfire, pero estas familiarizado con <a href="http://www.codeigniter.com" target="_blank">CodeIgniter</a>, podr&aacute;s iniciar y ejecutar el sistema en poco tiempo.</p>
 
-<p>If you're new to CodeIgniter, make sure you read through and understand the latest <a href="http://codeigniter.com/user_guide/" target="_blank">CodeIgniter User Guide</a> before diving into Bonfire. Your headaches will thank you.</p>
+<p>Si eres nuevo en CodeIgniter, asegurate de leer y entender la <a href="http://codeigniter.com/user_guide/" target="_blank">Gu&iacute;a del Usuario de CodeIgniter</a> (en ingl&eacute;s) antes de sumergirse en Bonfire.</p>
 
+<p>Si eres nuevo en Bonfire, puedes iniciar leyendo la <?php echo anchor('http://cibonfire.com/learn', 'documentaci&oacute;n', 'target="_blank"') ?> (en ingl&eacute;s).</p>
 
-<p>If you are new to Bonfire, you should start by reading the <?php echo anchor('http://cibonfire.com/learn', 'docs', 'target="_blank"') ?>.</p>
+<p>Bonfire utiliza Twitter Bootstrap para las interfaces (front-end y back-end). Aprende m&aacute;s <?php echo anchor('http://twitter.github.com/bootstrap/', 'aqu&iacute;', 'target="_blank"') ?> (en ingl&eacute;s).</p>
 
 <?php if (isset($current_user->email)) : ?>
 
 	<div class="alert alert-info" style="text-align: center">
-		<?php echo anchor(SITE_AREA, "Dive into Bonfire's Springboard"); ?>
+		<?php echo anchor(SITE_AREA, "Ingresa al &aacute;rea de administraci&oacute;n"); ?>
 	</div>
 
 <?php else :?>
