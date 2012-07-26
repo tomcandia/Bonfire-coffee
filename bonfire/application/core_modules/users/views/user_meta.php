d32282be42fd08e11ceb6318714858ed16b97eb3
Juan David Cardona <rocandante@gmail.com>
2012-07-26 16:47:54 -0500

    agregando departamentos y poniendo a colombia por defecto

--- bonfire/application/core_modules/users/views/user_meta.php
+++ bonfire/application/core_modules/users/views/user_meta.php
@@ -29,7 +29,7 @@
 						<label class="control-label" for="<?= $field['name'] ?>"><?php echo lang('user_meta_state'); ?></label>
 						<div class="controls">
 
-							<?php echo state_select(set_value($field['name'], isset($user->$field['name']) ? $user->$field['name'] : 'SC'), 'SC', 'US', $field['name'], 'span6 chzn-select'); ?>
+							<?php echo state_select(set_value($field['name'], isset($user->$field['name']) ? $user->$field['name'] : 'AN'), 'AN', 'CO', $field['name'], 'span6 chzn-select'); ?>
 
 						</div>
 					</div>
@@ -39,7 +39,7 @@
 					<div class="control-group <?php echo iif( form_error('country') , 'error'); ?>">
 						<label class="control-label" for="country"><?php echo lang('user_meta_country'); ?></label>
 						<div class="controls">
-							<?php echo country_select(set_value($field['name'], isset($user->$field['name']) ? $user->$field['name'] : 'US'), 'US', 'country', 'span6 chzn-select'); ?>
+							<?php echo country_select(set_value($field['name'], isset($user->$field['name']) ? $user->$field['name'] : 'CO'), 'CO', 'country', 'span6 chzn-select'); ?>
 
 						</div>
 					</div>
