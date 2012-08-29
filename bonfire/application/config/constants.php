ab5ad8d0abde8ed2678b307b4e587b993c0ab307
Juan David Cardona <rocandante@gmail.com>
2012-08-29 16:37:41 -0500

    actualizando la versión de bonfire-coffee

--- bonfire/application/config/constants.php
+++ bonfire/application/config/constants.php
@@ -37,7 +37,7 @@ define('FOPEN_WRITE_CREATE_STRICT',				'xb');
 define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
 
 
-define('BONFIRE_VERSION', 'v0.6-beta');
+define('BONFIRE_VERSION', 'v0.6-beta2');
 
 
 /*
